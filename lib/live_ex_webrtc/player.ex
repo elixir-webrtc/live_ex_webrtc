7a23bb1e28cbb6c787e89c3130f19da56a9f3903
Dmitro Szewczuk <dmitro.szewczuk@gmail.com>
2025-04-16 11:55:05 +0200

    Fix autoplay and settings button contrast (#20)
    * Remove autoplay and play manually on connection
    
    * Fix cog background

--- lib/live_ex_webrtc/player.ex
+++ lib/live_ex_webrtc/player.ex
@@ -231,7 +231,6 @@ defmodule LiveExWebRTC.Player do
           phx-hook="Player"
           class={["w-full h-full", @video_class]}
           controls
-          autoplay
           muted
         >
         </video>
@@ -269,7 +268,7 @@ defmodule LiveExWebRTC.Player do
 
         <button
           phx-click="toggle-settings"
-          class="absolute top-6 left-6 duration-300 ease-in-out group-hover:visible invisible transition-opacity opacity-0 group-hover:opacity-100 rounded-lg hover:bg-stone-800 p-2"
+          class="absolute top-6 left-6 duration-300 ease-in-out group-hover:visible invisible transition-opacity opacity-0 group-hover:opacity-100 rounded-lg bg-stone-700 hover:bg-stone-800 p-2"
         >
           <span class="hero-cog-8-tooth text-white w-6 h-6 block" />
         </button>
@@ -337,7 +336,7 @@ defmodule LiveExWebRTC.Player do
   @impl true
   def handle_info(
         {:ex_webrtc, pc, {:connection_state_change, :failed}},
-        %{assigns: %{player: %{pc: pc}}} 
+        %{assigns: %{player: %{pc: pc}}}
       ) do
     exit(:pc_failed)
   end
